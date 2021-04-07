d27e4862963b3414daf039ed45b2ffd59596157c
Damian Baldry <damianbaldry@gmail.com>
2021-04-07 22:43:44 +0100

    Tidy up

--- sdlanim.cpp
+++ sdlanim.cpp
@@ -1,11 +1,5 @@
-#include <iostream>
-//#include "stdafx.h"
-#include <SDL.h>
-#include <SDL_image.h>
-#include <chrono>
-#include "Scene1.h"
-#include "Menu.h"
 
+#include "Scene1.h"
 
 using namespace std;
 using namespace brightland;
@@ -14,11 +8,7 @@ using namespace brightland;
 
 int main(int argc, char* argv[])
 {
-
-   // Menu m;
-  //  m.menu();
-
-    Scene1 id;
-    id.scene1();
+    Scene1 game;
+    game.scene1();
     return 0;
 }
