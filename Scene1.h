abc0ed24ad5bdebd5bc0f7a1ef37f19867220d89
Damian Baldry <damianbaldry@gmail.com>
2021-04-10 17:27:20 +0100

    More tidying up

--- Scene1.h
+++ Scene1.h
@@ -29,9 +29,7 @@ namespace brightland {
 		static std::string objectToDestroy;
 		static std::string SceneTransitionStatement;
 
-		static int mouseHold;
 		static int tLoader;
-		static int messageHolder;
 		static bool mouseClick;
 		
 		//Inventory used variables. To check if the item has been used and shouldn't remain in the inventory.
@@ -43,12 +41,8 @@ namespace brightland {
 		static int action;
 		static int sceneHalt;
 
-
-		int colorkey;
-		int count;
 		int scene;
 		
-
 		//Set start position of main character.
 		static int xPosition;
 		static int yPosition;
@@ -66,6 +60,8 @@ namespace brightland {
 		SDL_Rect inv4;
 		SDL_Rect inv5;
 		SDL_Rect inv7;
+
+
 		static SDL_Rect dTexture;
 		SDL_Rect menu;
 		static SDL_Rect gdSprite;
@@ -77,48 +73,13 @@ namespace brightland {
 		//Texture used for displaying text.
 		static SDL_Texture* ftexture;
 
-		//Texture used for menu dialog.
-		SDL_Texture* sceneTexture;
-		SDL_Texture* texture;
-		SDL_Texture* rocks;
-		SDL_Texture* rocks2;
-		SDL_Texture* airBoxT;
-		SDL_Texture* computerScene1;
-		SDL_Texture* computerScene2;
-		SDL_Texture* wreakageScene;
-		SDL_Texture* scene1e;
-		SDL_Texture* scene1f;
-		SDL_Texture* scene1fa;
-		SDL_Texture* scene1fb;
-		SDL_Texture* scene1d;
-		SDL_Texture* scene1d2;
-
-		SDL_Texture* spriteTexture;
 		SDL_Texture* dialogTexture;
 		SDL_Texture* dialogmTexture;
 
-
-		//Scene Background Surfaces
-		SDL_Surface* imageSurface2;
-		SDL_Surface* imageSurface;
-		SDL_Surface* imageSurface3;
-		SDL_Surface* imageSurface4;
-		SDL_Surface* imageSurface5;
-		SDL_Surface* imageSurface6;
-		SDL_Surface* imageSurface7;
-		SDL_Surface* imageSurface8;
-		SDL_Surface* imageSurface9;
-		SDL_Surface* imageSurface10;
-		SDL_Surface* imageSurface11;
-		SDL_Surface* imageSurface12;
-
 		SDL_Surface* windowSurface;
-		//SDL_Surface* menuSurface;
-		SDL_Surface* purpleSurface;
-
+	
 		//Scene Dialog
 		SDL_Surface* dialog;
-		SDL_Surface* dialogm;
 		static SDL_Surface* fsurface;
 		static SDL_Renderer* renderer;
 
@@ -131,12 +92,7 @@ namespace brightland {
 
 	public: 
 		int scene1();
-		static void renderSprite();
 		void DoAction();
-
-		private:
-			
-
 
 	};
 
