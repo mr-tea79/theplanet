dba1d68f822f0357df7e4418f8c98bd62a434fad
Mr Tea <62661312+mr-tea79@users.noreply.github.com>
2021-06-08 16:03:03 +0100

    Text positioning improvements.

--- PlayerInteraction.cpp
+++ PlayerInteraction.cpp
@@ -14,28 +14,32 @@ int PlayerInteraction::playerMessage = 10000;
 void PlayerInteraction::textDimensions(int messageLength) {
 
 
-    if (Scene1::gdSprite.x > 130 && Scene1::gdSprite.x < 600 && Scene1::gdSprite.y >100) {
-        Scene1::textRect = { Scene1::gdSprite.x - 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
+  //  if (Scene1::gdSprite.x > 130 && Scene1::gdSprite.x < 600 && Scene1::gdSprite.y >100) {
+    //    Scene1::textRect = { Scene1::gdSprite.x - 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
         
-    }
-    else if (Scene1::gdSprite.x > 600 && Scene1::gdSprite.x < 700 && Scene1::gdSprite.y >100) {
-        Scene1::textRect = { Scene1::gdSprite.x - 400,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
+ //   }
+   // else if (Scene1::gdSprite.x > 600 && Scene1::gdSprite.x < 700 && Scene1::gdSprite.y >100) {
+  //      Scene1::textRect = { Scene1::gdSprite.x - 400,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
+
+  //  }
+    if (Scene1::gdSprite.x > 500 && Scene1::gdSprite.y >100 && messageLength > 40 ) {
+        Scene1::textRect = { Scene1::gdSprite.x - 600,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
 
     }
-    else if (Scene1::gdSprite.x > 700 && Scene1::gdSprite.y >100 && messageLength > 40 ) {
-        Scene1::textRect = { Scene1::gdSprite.x - 700,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
+    else {
+        Scene1::textRect = { Scene1::gdSprite.x - 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
 
     }
-    else if (Scene1::gdSprite.x < 130 && Scene1::gdSprite.y >100) {
-        Scene1::textRect = { Scene1::gdSprite.x + 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
+  //  else if (Scene1::gdSprite.x < 130 && Scene1::gdSprite.y >100) {
+  //      Scene1::textRect = { Scene1::gdSprite.x + 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
        
-    }
-    else if (Scene1::gdSprite.x > 680 && Scene1::gdSprite.y > 100) {
-        Scene1::textRect = { Scene1::gdSprite.x - 160 ,  Scene1::gdSprite.y - 80, messageLength * 10, 20 };       
-    }
-    else if (Scene1::gdSprite.y < 100) {
-        Scene1::textRect = { Scene1::gdSprite.x -300 ,  Scene1::gdSprite.y + 100, messageLength * 10, 20 };
-    }
+   // }
+   // else if (Scene1::gdSprite.x > 680 && Scene1::gdSprite.y > 100) {
+  //      Scene1::textRect = { Scene1::gdSprite.x - 160 ,  Scene1::gdSprite.y - 80, messageLength * 10, 20 };       
+ //   }
+ ///   else if (Scene1::gdSprite.y < 100) {
+  //      Scene1::textRect = { Scene1::gdSprite.x -300 ,  Scene1::gdSprite.y + 100, messageLength * 10, 20 };
+ //   }
    
 }
 
