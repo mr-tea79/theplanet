edf3cb6cc90e679861c885a85570e58d9678a6e9
Mr Tea <62661312+mr-tea79@users.noreply.github.com>
2022-08-21 22:12:27 +0100

    minor updates

--- Sound.cpp
+++ Sound.cpp
@@ -77,7 +77,7 @@ void Sound::playHoverSound() {
 
 void Sound::loadHoverSound() {
 	//mus = Mix_LoadMUS("Development Kits/Music/crunch-snow-sound-effect.mp3"); //Add your MP3 here for the background music.
-	hover = Mix_LoadWAV("Development Kits/Music/338229__fachii__button-hover.wav");
+	hover = Mix_LoadWAV("Development Kits/Music/button1.wav");
 }
 
 
@@ -89,33 +89,37 @@ void Sound::loadClickSound() {
 
 void Sound::loadMovementSounds() {
 	//mus = Mix_LoadMUS("Development Kits/Music/crunch-snow-sound-effect.mp3"); //Add your MP3 here for the background music.
-	walk = Mix_LoadWAV("Development Kits/Music/421128__giocosound__footstep-grass-4.wav");
+	walk = Mix_LoadWAV("Development Kits/Music/footstepsOut.mp3");
 }
 
 void Sound::loadSounds(std::string sceneBackground) {
 	if(Scene1::SceneBackground == "1" || Scene1::SceneBackground == "1fb" || Scene1::SceneBackground == "3b" || Scene1::SceneBackground == "1e"
 		|| Scene1::SceneBackground == "1f"){
 
-		mus2 = Mix_LoadMUS("Development Kits/Music/wind01.mp3"); //Add your MP3 here for the background music.
-		playSounds();
+	//	mus2 = Mix_LoadMUS("Development Kits/Music/wind01.mp3"); //Add your MP3 here for the background music.
+		//playSounds();
 	}
 
 	if (Scene1::SceneBackground == "3a") {
-		mus2 = Mix_LoadMUS("Development Kits/Music/sound-of-wind-blowing.mp3"); //Add your MP3 here for the background music.
-		playSounds();
+		//mus2 = Mix_LoadMUS("Development Kits/Music/sound-of-wind-blowing.mp3"); //Add your MP3 here for the background music.
+		//playSounds();
 	}
 
 	if (Scene1::SceneBackground == "3c" || Scene1::SceneBackground == "3d") {
-		mus2 = Mix_LoadMUS("Development Kits/Music/Water-dripping.mp3"); //Add your MP3 here for the background music.
-		playSounds();
+		//mus2 = Mix_LoadMUS("Development Kits/Music/Water-dripping.mp3"); //Add your MP3 here for the background music.
+		//playSounds();
 	}
 	if (Scene1::SceneBackground == "1b") {
-		mus2 = Mix_LoadMUS("Development Kits/Music/Electricity-sound-effect.mp3"); //Add your MP3 here for the background music.
-		playSounds();
+	//	mus2 = Mix_LoadMUS("Development Kits/Music/Electricity-sound-effect.mp3"); //Add your MP3 here for the background music.
+		//playSounds();
 	}
 	if (Scene1::SceneBackground == "1da") {
-		mus2 = Mix_LoadMUS("Development Kits/Music/modular-ambient-04-792.mp3"); //Add your MP3 here for the background music.
-		playSounds();
+		//mus2 = Mix_LoadMUS("Development Kits/Music/modular-ambient-04-792.mp3"); //Add your MP3 here for the background music.
+		//playSounds();
+	}
+	if (Scene1::SceneBackground == "0") {
+	//	mus2 = Mix_LoadMUS("Development Kits/Music/MenuBackgroundMusic.mp3"); //Add your MP3 here for the background music.
+	//	playSounds();
 	}
 
 }
