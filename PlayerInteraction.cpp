3e22a569f435ac3b8ba48353c44530acb3014f61
Mr Tea <62661312+mr-tea79@users.noreply.github.com>
2022-08-26 23:28:00 +0100

    Updated comments

--- PlayerInteraction.cpp
+++ PlayerInteraction.cpp
@@ -14,7 +14,7 @@ int PlayerInteraction::playerMessage = 100;
 void PlayerInteraction::textDimensions(int messageLength) {
 
     //Ternary operator
-    //(expr) ? <true logic> : <false logic>
+    //(expr) ? <true logic> : <false logic>  (IF TRUE ? DO THIS : ELSE DO THIS)
     //Used to reduce lots of IF statements.
 
     Scene1::gdSprite.x > 550 && Scene1::gdSprite.y > 100 && messageLength > 40 ? Scene1::textRect = { Scene1::gdSprite.x - 300,  Scene1::gdSprite.y - 100, messageLength * 10, 20 } : Scene1::textRect = { Scene1::gdSprite.x - 60,  Scene1::gdSprite.y - 100, messageLength * 10, 20 };
@@ -109,7 +109,7 @@ std::string PlayerInteraction::DisplayPlayerMessages() {
     std::string interactionMessage;
     
     //Ternary operator
-   //(expr) ? <true logic> : <false logic>
+   //(expr) ? <true logic> : <false logic>  (IF TRUE ? DO THIS : ELSE DO THIS)
    //Used to reduce lots of IF statements.
 
     //This will show the scene player messages in sequence.
