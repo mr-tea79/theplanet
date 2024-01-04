74c7df86e212a1a5313097b91e22e7690a58a6af
Mr Tea <62661312+mr-tea79@users.noreply.github.com>
2024-01-04 18:22:10 +0000

    Update PlayerMovement.cpp

--- PlayerMovement.cpp
+++ PlayerMovement.cpp
@@ -408,11 +408,10 @@ int PlayerMovement::walky(int x, int y, int gd, int gy, int screenWidth, int scr
         ObjectPositions op;
         
         //Increase and decrease player size depending on location on screen.
-        Scene1::yp > 55 && spriteSizeLock !=3 ? Scene1::SPRITE_SIZE = op.CalcAssetSize(Scene1::SPRITE_SIZE,5),spriteSizeLock ++ : move = 0;
+        Scene1::yp > 55 && spriteSizeLock !=2 ? Scene1::SPRITE_SIZE = op.CalcAssetSize(Scene1::SPRITE_SIZE,5),spriteSizeLock ++ : move = 0;
         Scene1::yp < 53 && spriteSizeLock >=0 ? Scene1::SPRITE_SIZE = op.CalcAssetDecreaseSize(Scene1::SPRITE_SIZE,3), spriteSizeLock-- : move = 0;
       
-        //TEST
-        
+              
         gy >= y -10 && Scene1::yp < 68 && Scene1::yp > 48 ? gy = doYWalkUp(gy), position++ : Scene1::playerIsMoving = 0;
         gy <= y -50 && Scene1::yp < 68 && Scene1::yp < 60 ? gy = doYWalkDown(gy), position++ : Scene1::playerIsMoving = 0;
         
